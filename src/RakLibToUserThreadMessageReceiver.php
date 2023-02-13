26ed56fa9db06e4ca6e8920c0ede2e01e219bb9c
Dylan K. Taylor <dktapps@pmmp.io>
2023-02-13 13:40:30 +0000

    Ignore PHPStan error properly
    I don't like using ignore-line because it silences _all_ errors that could appear on a line, which is a recipe for missing future bugs.

--- src/RakLibToUserThreadMessageReceiver.php
+++ src/RakLibToUserThreadMessageReceiver.php
@@ -69,7 +69,7 @@ final class RakLibToUserThreadMessageReceiver{
 				$sessionId = Binary::readInt(substr($packet, $offset, 4));
 				$offset += 4;
 				$reason = ord($packet[$offset]);
-				$listener->onClientDisconnect($sessionId, $reason); // @phpstan-ignore-line
+				$listener->onClientDisconnect($sessionId, $reason);
 			}elseif($id === ITCProtocol::PACKET_ACK_NOTIFICATION){
 				$sessionId = Binary::readInt(substr($packet, $offset, 4));
 				$offset += 4;
